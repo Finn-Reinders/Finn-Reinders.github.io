c3aedcebd9d0c2f25f86f41fa2e6e2c1cbdb8f32
Finn-Reinders <finn.reinders@sintlucasedu.nl>
2025-02-26 13:12:04 +0100

    added different pages

---
+++ Practice/functioning/subscriptionsscript.js
@@ -0,0 +1,16 @@
+let menuOpened = true;
+function openMenu() {
+
+    if (menuOpened === false)
+    {
+        document.getElementById("menuLogo").style.height = "300px";
+        menuOpened = true;
+        document.getElementById("menuItemsId").style.display = "block";
+    }
+    else if (menuOpened === true)
+    {
+        document.getElementById("menuLogo").style.height = "75px";
+        menuOpened = false;
+        document.getElementById("menuItemsId").style.display = "none";
+    }
+}
