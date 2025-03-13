b58e4f8211820628d61ff11bc831719330f17337
Finn-Reinders <finn.reinders@sintlucasedu.nl>
2025-03-13 20:16:58 +0100

    added custom cursor

--- Practice/functioning/script.js
+++ Practice/functioning/script.js
@@ -1,25 +1,32 @@
-window.onload = function() {
+window.onload = function () {
     let element = document.getElementById("loader");
-    element.style.animation = "loading 2s ease-in-out 1";
-addEventListener("animationend", function () {
-    element.classList.remove("loading");
-    element.style.animation = "loaded .8s linear 1 forwards";
-    setTimeout(function ()
-    {
-        document.getElementById("loaderScreen").classList.add("loaded");
+    element.style.animation = "loading 1.5s ease-in-out 1";
+    addEventListener("animationend", function () {
+        element.classList.remove("loading");
+        element.style.animation = "loaded .8s linear 1 forwards";
+        setTimeout(function () {
+            document.getElementById("loaderScreen").classList.add("loaded");
 
 
-
-    }, 800);
-    document.getElementById("loader").addEventListener("transitionend", function ()
-    {
-        document.body.removeChild(document.querySelector(".loader"));
+        }, 800);
+        document.getElementById("loader").addEventListener("transitionend", function () {
+            document.body.removeChild(document.querySelector(".loader"));
+        })
     })
-})
 };
 
-document.addEventListener("DOMContentLoaded", () =>
-{
+document.addEventListener("mousemove", (event) => {
+    document.getElementById("cursor").style.transform = `translate(${event.clientX - 12.5}px, ${event.clientY - 12.5}px)`;
+    const topBar = document.getElementById("topBar");
+    const cursor = document.getElementById("cursor");
+
+    topBar.addEventListener("mouseenter", () => {
+        cursor.classList.add("hover");
+    })
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
     const home = document.getElementById("home");
     const background = document.getElementById("background");
     const backgroundColors = {
@@ -32,11 +39,11 @@ document.addEventListener("DOMContentLoaded", () =>
         item7: "radial-gradient(circle, rgba(71,124,178,1) 0%, rgba(115,112,117,1) 84%, rgba(5,1,9,1) 100%)",
         item8: "radial-gradient(circle, rgba(22,19,16,1) 0%, rgba(49,49,54,1) 100%)",
         item9: "radial-gradient(circle, rgba(214,226,241,1) 0%, rgba(98,118,67,1) 100%)",
-        item10: "radial-gradient(circle, rgba(75,62,37,1) 0%, rgba(63,52,37,1) 99%)",
+        item10: "radial-gradient(circle, rgba(102,89,61,1) 0%, rgba(13,11,8,1) 100%)",
+
     };
 
-    function duplicateItems()
-    {
+    function duplicateItems() {
         const items = Array.from(home.children);
         items.forEach(item => {
             let clone = item.cloneNode(true);
@@ -47,12 +54,9 @@ document.addEventListener("DOMContentLoaded", () =>
     duplicateItems();
 
     home.addEventListener("scroll", () => {
-        if (home.scrollLeft === 0)
-        {
+        if (home.scrollLeft === 0) {
             home.scrollLeft = home.scrollWidth / 2;
-        }
-        else if (home.scrollLeft + home.clientWidth >= home.scrollWidth - 1)
-        {
+        } else if (home.scrollLeft + home.clientWidth >= home.scrollWidth - 1) {
             home.scrollLeft = home.scrollWidth / 4;
         }
         updateCenterItem();
@@ -80,8 +84,7 @@ document.addEventListener("DOMContentLoaded", () =>
         items.forEach(item => item.classList.remove("enlarged"));
         if (closestItem) closestItem.classList.add("enlarged");
 
-        if (closestItem && closestItem.id in backgroundColors)
-        {
+        if (closestItem && closestItem.id in backgroundColors) {
             fadeBackground(backgroundColors[closestItem.id]);
         }
     }
@@ -94,17 +97,23 @@ document.addEventListener("DOMContentLoaded", () =>
             background.style.opacity = 1;
         }, 500);
     }
+
     updateCenterItem();
 });
 
 let menuOpened = true;
-function homePage()
-{
 
-        document.getElementById("home").style.display = "flex";
-        document.getElementById("aboutUs").style.display = "none";
-        document.getElementById("contact").style.display = "none";
-        document.getElementById("subscriptions").style.display = "none";
+function homePage() {
+
+    document.getElementById("home").style.display = "flex";
+    document.getElementById("aboutUs").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("subscriptions").style.display = "none";
+
+    // document.getElementById("home").style.animation = "appear 3s";
+    // document.getElementById("aboutUs").style.animation = "disappear 3s";
+    // document.getElementById("contact").style.animation = "disappear 3s";
+    // document.getElementById("subscriptions").style.animation = "disappear 3s";
 
     document.getElementById("homeUnderline").style.width = "100%";
     document.getElementById("aboutUsUnderline").style.width = "0";
@@ -115,12 +124,16 @@ function homePage()
     openMenu();
 }
 
-function aboutUsPage()
-{
+function aboutUsPage() {
     document.getElementById("home").style.display = "none";
     document.getElementById("aboutUs").style.display = "flex";
     document.getElementById("contact").style.display = "none";
     document.getElementById("subscriptions").style.display = "none";
+
+    // document.getElementById("home").style.animation = "disappear 3s";
+    // document.getElementById("aboutUs").style.animation = "appear 3s";
+    // document.getElementById("contact").style.animation = "disappear 3s";
+    // document.getElementById("subscriptions").style.animation = "disappear 3s";
 
     document.getElementById("homeUnderline").style.width = "0";
     document.getElementById("aboutUsUnderline").style.width = "100%";
@@ -131,12 +144,16 @@ function aboutUsPage()
     openMenu();
 }
 
-function contactPage()
-{
+function contactPage() {
     document.getElementById("home").style.display = "none";
     document.getElementById("aboutUs").style.display = "none";
     document.getElementById("contact").style.display = "flex";
     document.getElementById("subscriptions").style.display = "none";
+
+    // document.getElementById("home").style.animation = "disappear 3s";
+    // document.getElementById("aboutUs").style.animation = "disappear 3s";
+    // document.getElementById("contact").style.animation = "appear 3s";
+    // document.getElementById("subscriptions").style.animation = "disappear 3s";
 
     document.getElementById("homeUnderline").style.width = "0";
     document.getElementById("aboutUsUnderline").style.width = "0";
@@ -147,12 +164,16 @@ function contactPage()
     openMenu();
 }
 
-function subscriptionsPage()
-{
+function subscriptionsPage() {
     document.getElementById("home").style.display = "none";
     document.getElementById("aboutUs").style.display = "none";
     document.getElementById("contact").style.display = "none";
     document.getElementById("subscriptions").style.display = "flex";
+
+    // document.getElementById("home").style.animation = "disappear 3s";
+    // document.getElementById("aboutUs").style.animation = "disappear 3s";
+    // document.getElementById("contact").style.animation = "disappear 3s";
+    // document.getElementById("subscriptions").style.animation = "appear 3s";
 
     document.getElementById("homeUnderline").style.width = "0";
     document.getElementById("aboutUsUnderline").style.width = "0";
@@ -164,16 +185,14 @@ function subscriptionsPage()
 }
 
 
+
 function openMenu() {
 
-    if (menuOpened === true)
-    {
+    if (menuOpened === true) {
         document.getElementById("menuLogo").style.top = "0px";
         menuOpened = false;
 
-    }
-    else if (menuOpened === false)
-    {
+    } else if (menuOpened === false) {
         document.getElementById("menuLogo").style.top = "-225px";
         menuOpened = true;
     }
